dfdcf6c872191bad8112bcb00952bf18d5ddcbd6
RichardCT <rjct1997@gmail.com>
2022-03-30 14:11:36 -0600

    Add files via upload

---
+++ source/state_machine.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "board.h"
+#include "fsl_debug_console.h"
+#include "fsl_gpio.h"
+
+#include "clock_config.h"
+#include "pin_mux.h"
+#include "fsm_gpio.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define BOARD_LED_GPIO     BOARD_LED_RED_GPIO
+#define BOARD_LED_GPIO_PIN BOARD_LED_RED_GPIO_PIN
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+
+
+
+	/* Define the init structure for the output LED pin*/
+	gpio_pin_config_t led_config = {
+	    kGPIO_DigitalOutput,
+	    0,
+	};
+
+    /* Board pin, clock, debug console init */
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    /* Print a note to terminal. */
+    PRINTF("\r\n GPIO Driver example\r\n");
+    PRINTF("\r\n The LED is blinking.\r\n");
+
+    /* Init output LED GPIO. */
+    GPIO_PinInit(BOARD_LED_GPIO, BOARD_LED_GPIO_PIN, &led_config);
+
+    while (1)
+    {
+    	/* Repeat the sequence depending of present State */
+    	FSM_SelectState();
+    }
+
+    return 0;
+}
